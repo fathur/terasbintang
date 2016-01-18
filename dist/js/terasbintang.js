eee66451670c435b9d3ce73d854cafaf3205735a
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2016-01-18 21:51:33 +0700

    adjusment in mobile devices

--- dist/js/terasbintang.js
+++ dist/js/terasbintang.js
@@ -1,9 +1,28 @@
+var maxExtraSmallDeviceWidth    = 767,
+    minSmallDeviceWidth         = maxExtraSmallDeviceWidth + 1,
+    maxSmallDeviceWidth         = 991,
+    minMediumdDeviceWidth       = maxSmallDeviceWidth + 1,
+    maxMediumDeviceWidth        = 1199,
+    minLargeDeviceWidth         = maxMediumDeviceWidth + 1,
+    headTopHeight;
+
+if($(window).width() > maxExtraSmallDeviceWidth) {
+    headTopHeight = $('#head-top').height() + 40;
+} else {
+    headTopHeight = $('#head-top').height() + 40;
+
+}
+
 $('#header-menu').affix({
     offset: {
-        top: $('#head-top').height() + 40
+        top: headTopHeight
     }
 });
 
-$('.navbar-teras .navbar-nav > li > a').css({
-    width: '136px'
-});
+if($(window).width() > maxExtraSmallDeviceWidth) {
+
+
+    /*$('.navbar-teras .navbar-nav > li > a').css({
+        width: '136px'
+    });*/
+}
